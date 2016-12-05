d976673ee67c3c1d77f7f661c393e2fd85bab4ad
goodmorningnelson <mtr.ruehr@gmail.com>
2016-12-05 13:29:22 -0500

    ok

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ angular.module('online-resume').controller('appCtrl',function ($scope) {
     //angular media querry
     $scope.infoItemModel1 = true;
     $scope.infoItemModel2 = true;
+    $scope.infoItemModel3 = true;
+    $scope.infoItemModel4 = true;
 
     $scope.workItemModel1 = true;
 });
