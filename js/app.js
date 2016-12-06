8b82ebbd05e3a34a4d7d900d39f87a40b603c2fb
goodmorningnelson <mtr.ruehr@gmail.com>
2016-12-06 12:53:00 -0500

    safari fix + color change

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 //need controller for things to work.
 angular.module('online-resume', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-angular.module('online-resume').controller('appCtrl',function ($scope) {
+angular.module('online-resume').controller('appCtrl',['$scope','$window',function ($scope,$window) {
     //angular media querry
+    console.log($window.width,$window);
+
     $scope.infoItemModel1 = true;
     $scope.infoItemModel2 = true;
     $scope.infoItemModel3 = true;
     $scope.infoItemModel4 = true;
 
     $scope.workItemModel1 = true;
-});
+}]);
+
+
+//$window.width
