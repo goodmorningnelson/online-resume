e611075ebd2b0685751dc2232f4b281949f042f6
goodmorningnelson <mtr.ruehr@gmail.com>
2016-12-06 13:24:01 -0500

    js fix

--- js/app.js
+++ js/app.js
@@ -11,6 +11,14 @@ angular.module('online-resume').controller('appCtrl',['$scope','$window',functio
     $scope.infoItemModel4 = true;
 
     $scope.workItemModel1 = true;
+    $scope.workItemModel2 = true;
+    $scope.workItemModel3 = true;
+    $scope.workItemModel4 = true;
+    $scope.workItemModel5 = true;
+    $scope.workItemModel6 = true;
+    $scope.workItemModel7 = true;
+    $scope.workItemModel8 = true;
+    $scope.workItemModel9 = true;
 }]);
 
 
